08e87f9eb19619a98fd79b96bdb470204d346965
Ty Dang <ty.dang1110@gmail.com>
2020-01-01 17:40:57 -0500

    oneschema now is usable as a todo list

--- oneschema/scripts.js
+++ oneschema/scripts.js
@@ -2,7 +2,7 @@ var noteElements = document.getElementsByTagName("article");
 var notesElement = document.getElementById("notes");
 var noteTemplate = document.getElementById("noteTemplate");
 var noteInput = document.getElementById("noteInput");
-var orderInput = document.getElementById("orderInput");
+//var orderInput = document.getElementById("orderInput");
 var keyboardListener = document.addEventListener("keypress", keyboard);
 var dropListener = document.addEventListener("drop", onDrop);
 
@@ -39,17 +39,21 @@ function clearDisplay() {
 function keyboard(e) {
     if (e.keyCode == 13) {
         e.preventDefault();
-        addNote();
+        if (e.target == noteInput) {
+            addNote();
+        } else if (e.target.tagName == "TEXTAREA") {
+            saveNote(e);
+        }
     }
 }
 
 function addNote() {
     add({
         "note": ""+noteInput.value,
-        "order": parseInt(orderInput.value)
+        //"order": parseInt(orderInput.value)
     });
     noteInput.value = '';
-    orderInput.value = '';
+    //orderInput.value = '';
     save();
     load();
     clearDisplay();
@@ -61,6 +65,17 @@ function addNote() {
 function deleteNote(e) {
 	remove(e.target.parentElement.getAttribute("id"));
 	save();
+	load();
+	clearDisplay();
+	order();
+	orderRegister();
+	render();
+}
+
+function saveNote(e) {
+    var id = e.target.parentElement.getAttribute("id");
+    set(id, {"note": e.target.value});
+    save();
 	load();
 	clearDisplay();
 	order();
