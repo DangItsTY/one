5193b0f15e0261c29fcc955c01dfbc4bb6831c78
Ty Dang <ty.dang1110@gmail.com>
2020-01-12 13:19:51 -0500

    add onelist deadline filter

--- onelist/scripts.js
+++ onelist/scripts.js
@@ -1,12 +1,9 @@
 function filter() {
-	console.log(dataNotes);
 	// for now simply hide elements that are not pertinent
 	var now = Date.now();
 	for (var i = 0; i < dataNotes.length; i++) {
-		console.log(dataNotes[i]["deadline"]);
-		var deadline = new Date(dataNotes[i]["deadline"]);
-		console.log(deadline);
-		if (!dataNotes[i]["deadline"] || dataNotes[i]["deadline"] > now) {
+		var deadline = dataNotes[i]["deadline"] ? new Date(dataNotes[i]["deadline"]) : dataNotes[i]["deadline"];
+		if (!deadline || deadline > now) {
 			document.getElementById(dataNotes[i]["id"]).setAttribute("hidden", true);
 		}
 	}
