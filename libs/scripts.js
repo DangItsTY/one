e9b4d6af6b8699086495ba604a0f702744416864
Ty Dang <ty.dang1110@gmail.com>
2020-01-12 12:21:02 -0500

    initial onelist

---
+++ libs/scripts.js
@@ -0,0 +1,22 @@
+var noteElements = document.getElementsByName("onenote");
+var notesElement = document.getElementById("notes");
+var noteTemplate = document.getElementById("noteTemplate");
+
+function initialize() {
+    load();
+    render();
+}
+initialize();
+
+function render() {
+    for (var i = 0; i < dataNotes.length; i++) {
+        var elementGhost = noteTemplate.content.cloneNode(true);
+        notesElement.appendChild(elementGhost);
+        var newElement = noteElements[noteElements.length - 1];
+        newElement.setAttribute("id", dataNotes[i].id);
+		for (var j = 0; j < newElement.children.length; j++) {
+			var key = newElement.children[j].getAttribute("name");
+			newElement.children[j].value ? newElement.children[j].value = dataNotes[i][key] : newElement.children[j].innerHTML = dataNotes[i][key];
+		}
+    }
+}
