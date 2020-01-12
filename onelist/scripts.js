aeac3c87172bb2392d3f41b0b13495c5a456057b
Ty Dang <ty.dang1110@gmail.com>
2020-01-12 15:06:37 -0500

    stylized onelist for alertness

--- onelist/scripts.js
+++ onelist/scripts.js
@@ -1,10 +1,10 @@
 function filter() {
-	// for now simply hide elements that are not pertinent
+	// remove elements not pertinent
 	var now = Date.now();
 	for (var i = 0; i < dataNotes.length; i++) {
 		var deadline = dataNotes[i]["deadline"] ? new Date(dataNotes[i]["deadline"]) : dataNotes[i]["deadline"];
 		if (!deadline || deadline > now) {
-			document.getElementById(dataNotes[i]["id"]).setAttribute("hidden", true);
+		    notesElement.removeChild(document.getElementById(dataNotes[i]["id"]));
 		}
 	}
 }
