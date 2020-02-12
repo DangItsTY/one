8dd6ac26ff1480e0e28ba4d1b686b2dd3113fcab
Ty Dang <ty.dang1110@gmail.com>
2020-02-11 19:54:40 -0500

    updated onelist now timing to offset timezone

--- onelist/scripts.js
+++ onelist/scripts.js
@@ -2,7 +2,10 @@ render();
 
 function filter() {
 	// remove elements not pertinent
-	var now = Date.now();
+	var date = new Date();
+	var userTimezoneOffset = date.getTimezoneOffset();
+	var now = new Date(date.getTime() - (userTimezoneOffset * 60000));
+
 	for (var i = 0; i < dataNotes.length; i++) {
 		var deadline = dataNotes[i]["deadline"] ? new Date(dataNotes[i]["deadline"]) : dataNotes[i]["deadline"];
 		if (!deadline || deadline > now) {
