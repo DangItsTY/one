56342906953e96f85ec87fcbcf315c58e8b81462
Ty Dang <ty.dang1110@gmail.com>
2020-07-18 10:07:05 -0400

    see the future

--- onelist/scripts.js
+++ onelist/scripts.js
@@ -1,14 +1,15 @@
+var endDate = new Date();
+
 initialize();
 
 render();
 
 function filter() {
 	// remove elements not pertinent
-	var now = new Date();
 
 	for (var i = 0; i < dataNotes.length; i++) {
 		var deadline = dataNotes[i]["deadline"] ? new Date(dataNotes[i]["deadline"]) : dataNotes[i]["deadline"];
-		if (!deadline || deadline > now) {
+		if (!deadline || deadline > endDate) {
 		    notesElement.removeChild(document.getElementById(dataNotes[i]["id"]));
 		}
 	}
@@ -16,11 +17,7 @@ function filter() {
 filter();
 
 function saveNote(e) {
-    var dayNum = 0; // this could be a parameter, but I always want sundays
-    var date = new Date();
-    var daysToNextWeekDay = 7 + dayNum - date.getDay();
-
-    var sunday = new Date(date.getFullYear(), date.getMonth(), date.getDate() + daysToNextWeekDay);
+    var sunday = nextWeek(new Date(), 0);
     var newDeadline = sunday.toISOString();
 
     var id = e.target.parentElement.getAttribute("id");
@@ -42,4 +39,23 @@ function deleteNote(e) {
 	clearDisplay();
 	render();
 	filter();
+}
+
+function nextWeek(date, day) {
+  //  returns the next week given the date (usually the now date) and day in integer
+  //  0 is sunday and 6 is saturday
+  //  note: next week can mean this or following or coming up, it's literally next
+  var daysToNextWeekDay = (7 + (day - date.getDay())) % 7;
+  daysToNextWeekDay = daysToNextWeekDay === 0 ? 7 : daysToNextWeekDay;
+  return new Date(date.getFullYear(), date.getMonth(), date.getDate() + daysToNextWeekDay);
+}
+
+//  Load to the next sunday, additive
+function future() {
+  endDate = nextWeek(endDate, 0);
+  
+  load();
+  clearDisplay();
+  render();
+  filter();
 }
