0c188df8d1c46fc8cf8d59bb590c8631a1a55a6e
Ty Dang <ty.dang1110@gmail.com>
2020-08-30 20:54:18 -0400

    added vertical movement for textboard

--- textboard/scripts.js
+++ textboard/scripts.js
@@ -1,9 +1,11 @@
 var noteElements = document.getElementsByTagName("textarea");
 var textboardElement = document.getElementById("textboard");
 var noteTemplate = document.getElementById("noteTemplate");
-var keyboardListener = document.addEventListener("keypress", navigationKeyboard);
+var keyboardListener = document.addEventListener("keydown", navigationKeyboard);
 var currentFocus;
 var idCount = 1;
+var numPerRow = window.innerWidth <= 1597 ? 7 : 11;
+console.log(numPerRow);
 
 function initialize() {
     for (var i = 0; i < noteElements.length; i++) {
@@ -22,10 +24,16 @@ function navigationKeyboard(e) {
     if (e.keyCode == 13 && !e.shiftKey) {
         e.preventDefault();
         //noteRefresh(currentFocus);
-        navigateRight(currentFocus);
+        navigateRight();
     } else if (e.keyCode == 13 && e.shiftKey) {
         e.preventDefault();
         navigateLeft();
+    } else if (e.keyCode == 9 && !e.shiftKey) {
+      e.preventDefault();
+      navigateDown();
+    } else if (e.keyCode == 9 && e.shiftKey) {
+      e.preventDefault();
+      navigateUp();
     }
 }
 
@@ -67,6 +75,22 @@ function navigateRight() {
 function navigateLeft() {
     currentFocus--;
     if (currentFocus < 0) {
+        currentFocus = 0;
+    }
+    noteElements[currentFocus].focus();
+}
+
+function navigateDown() {
+    currentFocus += numPerRow;
+    if (noteElements[currentFocus] === undefined) {
+        currentFocus = noteElements.length - 1;
+    }
+    noteElements[currentFocus].focus();
+}
+
+function navigateUp() {
+    currentFocus -= numPerRow;
+    if (noteElements[currentFocus] === undefined) {
         currentFocus = 0;
     }
     noteElements[currentFocus].focus();
